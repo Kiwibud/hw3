b7cac92c92852786a7c2d231758611f8214cb0d6
Grace To <tps.sang@gmail.com>
2019-09-09 13:42:40 -0700

    Update blurimage.py

--- blurimage.py
+++ blurimage.py
@@ -45,46 +45,6 @@ def average_matrix(m):
 
 
 # min, max, list slice, list comprehension
-# def box(m, center):
-#     """
-
-#     :param m:
-#     :param center:
-#     :return:
-#     """
-#     grid = []
-#     if len(m) == 0:
-#         return None
-#     if len(m) == 1:
-#         i, j = 0, 0
-#     else:
-#         i = center[0]
-#         j = center[1]
-#     if j-1 >= 0:
-#         # check if row 1 of the box exist
-#         if i-1 >= 0:
-#             # check for right limit
-#             g1 = m[i-1][j-1:j+2] if j+1 < len(m[0]) else m[i-1][j-1:j+1]
-#             grid.append(g1)  # append row 1
-#         g2 = m[i][j-1:j+2] if j+1 < len(m[0]) else m[i][j-1:j+1]
-#         grid.append(g2)  # append row 2
-#         if i+1 < len(m):
-#             g3 = m[i+1][j-1:j+2] if j+1 < len(m[0]) else m[i+1][j-1:j+1]
-#             grid.append(g3)  # append row 3
-#     else:
-#         if i-1 >= 0:
-#             g1 = m[i-1][j:j+2] if j+1 < len(m[0]) else m[i-1][j:j+1]
-#             grid.append(g1)
-#         g2 = m[i][j:j+2] if j+1 < len(m[0]) else m[i][j:j+1]
-#         grid.append(g2)
-#         if i+1 < len(m):
-#             g3 = m[i+1][j:j+2] if j+1 < len(m[0]) else m[i+1][j:j+1]
-#             grid.append(g3)
-
-#     return grid
-
-
-# min, max, list slice, list comprehension
 def box(m: list, center: tuple) -> list:
     py, px = center[0], center[1]
     m = m if isinstance(m[py], list) else [m]
@@ -94,27 +54,6 @@ def box(m: list, center: tuple) -> list:
         row_len = len(row)
         output.append(row[max(px - 1, 0):min(px + 2, row_len)])
     return output
-
-
-def box2(m, center):
-    y, x = center[0], center[1]  # Prof stated these are always valid
-    m = m if type(m[y]) == list else [m]  # Wrap single-array as 2D.
-    output = []
-    # Ternary operators -- handling out of bounds checking.
-    range_y = 1 + (y if y >= len(m) - 1 else y + 1)
-    for y in range(0 if y <= 0 else y - 1, range_y):
-        output.append(get_row2(m, x, y))
-    return output
-
-
-# Retrieves up to three elements from a center point within a row.
-def get_row2(matrix: list, x: int, y: int) -> list:
-    row = []
-    # No need to check bounds here -- already done in 'box()'
-    range_x = 1 + (x if x >= len(matrix[y]) - 1 else x + 1)
-    for x in range(0 if x <= 0 else x - 1, range_x):
-        row.append(matrix[y][x])
-    return row
 
 
 # return integer, invoke average and box function
